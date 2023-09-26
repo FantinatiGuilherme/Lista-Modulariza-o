f8b9e3207e9c16bd644e8d0eaf89e47c70aad2be
FantinatiGuilherme <114171437+FantinatiGuilherme@users.noreply.github.com>
2023-09-25 21:31:17 -0300

    Atualizar o Lista Modularização - 04.c

--- Lista Modularização - 04.c
+++ Lista Modularização - 04.c
@@ -83,5 +83,76 @@ void operacao (){
 
 }
 }
+ 
+    setlocale(LC_ALL, "Portuguese");
+    int escolha=0;
+    
+    entrada();
+    
+    
+    operacao();
+    
+    
+    
+    
+
+    return 0;
+}
+
+void entrada(){
+    setlocale(LC_ALL, "Portuguese");
+    printf("---------------------- BANCO ----------------------\n");
+    printf("Digite o número para realizar as seguintes operações:\n1 - Deposito\n2 - Saque\n3 - Saldo\n4 - Sair\n");
+   printf("---------------------------------------------------\n");
+}
+
+
+int leia(){
+    setlocale(LC_ALL, "Portuguese");
+    int a;
+    
+    printf("Digite a operação desejada : ");
+    
+    scanf("%d", &a);
+    
+    while(a <= 0 || a > 4 ){
+        printf("Não existe nenhuma operação com esse número, digite novamente : ");
+        scanf("%d",&a);
+    }
+    return a;
+}
+
+void operacao (){
+    setlocale(LC_ALL, "Portuguese");
+    float valor, saldo = 0;
+    int escolha = 0;
+    
+    while(escolha != 4){
+        escolha = leia();
+    
+    switch(escolha){
+        case 1:
+            printf("Qual valor do seu deposito : R$ ");
+            scanf("%f", &valor);
+            saldo += valor;
+            printf("Operação realizada com sucesso! \n\n\n");
+            break;
+        case 2:
+            printf("Digite o valor que vai ser sacado : R$ ");
+            scanf("%f", &valor);
+            
+            saldo -= valor;
+            printf("Operação realizada com sucesso!\n\n\n");
+            break;
+        
+        case 3:
+        printf("Saldo da conta : R$ %.2f\n\n",saldo);
+        break;
+        
+    
+        }
+
+}
+}
 
 
